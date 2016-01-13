328469ad4688208021bbd6a5d91549468272d798
hackt0r <gravmatt@gmail.com>
2016-01-13 10:40:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# js-notify
+
+Wrapper for the Notification object.
+
+Works on Chrome, Safari and Firefox.
+
+Maybe on Opera, IE an Edge as well.
+
+If someone tested it on other browser please let me know.
+
+## How it works
+
+```
+notify('title', {
+    body: 'Notification Text',
+    icon: 'path/to/image.png',
+    onclick: function(e) {}, // e -> Notification object
+    onclose: function(e) {},
+    ondenied: function(e) {}
+  });
+```
+
+You just have to set a title to make it work.
+
+The json object on the second argument is optional.
+
+If the function runs for the first time, is asks the user for permissions.
+
+Follow me on [Twitter @gravmatt](https://twitter.com/gravmatt).
---
+++ notify.js
@@ -0,0 +1,33 @@
+var notify = function (title, options) {
+    var guid = function() {
+        function s4() {
+          return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+      };
+
+    if (!'Notification' in window) {
+        return;
+    }
+    if (Notification.permission === 'default') {
+        Notification.requestPermission(function () {
+            notify(title, options, onclick, onclose, onDenied);
+        });
+    }
+    else if (Notification.permission === 'granted') {
+        opt = options || {}
+        opt.tag = guid()
+        var n = new Notification(title, opt);
+
+        n.onclick = function () {
+            opt.onclick && opt.onclick(this);
+            this.close();
+        };
+        n.onclose = function () {
+            opt.onclose && opt.onclose(this);
+        };
+    }
+    else if (Notification.permission === 'denied') {
+        (options && options.ondenied) && options.ondenied(this);
+    }
+};
